461043d10d31eceaa7bddc42bc14931a3ee4308c
byasov <byasovdavid@bk.ru>
2022-06-27 22:59:38 +0300

    Добавьте файлы проекта.

---
+++ animals4/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace animals4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonTrain = new System.Windows.Forms.Button();
+            this.buttonTest = new System.Windows.Forms.Button();
+            this.labelMode = new System.Windows.Forms.Label();
+            this.labelWord = new System.Windows.Forms.Label();
+            this.AnimalsPicture = new System.Windows.Forms.PictureBox();
+            this.buttonQuestion2 = new System.Windows.Forms.Button();
+            this.buttonQuestion1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.AnimalsPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonTrain
+            // 
+            this.buttonTrain.Location = new System.Drawing.Point(293, 117);
+            this.buttonTrain.Name = "buttonTrain";
+            this.buttonTrain.Size = new System.Drawing.Size(73, 23);
+            this.buttonTrain.TabIndex = 0;
+            this.buttonTrain.Text = "Обучение";
+            this.buttonTrain.UseVisualStyleBackColor = true;
+            this.buttonTrain.Click += new System.EventHandler(this.buttonTrain_Click);
+            // 
+            // buttonTest
+            // 
+            this.buttonTest.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonTest.Location = new System.Drawing.Point(451, 117);
+            this.buttonTest.Name = "buttonTest";
+            this.buttonTest.Size = new System.Drawing.Size(86, 23);
+            this.buttonTest.TabIndex = 1;
+            this.buttonTest.Text = " Тест";
+            this.buttonTest.UseVisualStyleBackColor = true;
+            this.buttonTest.Click += new System.EventHandler(this.buttonTest_Click);
+            // 
+            // labelMode
+            // 
+            this.labelMode.AutoSize = true;
+            this.labelMode.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelMode.Location = new System.Drawing.Point(287, 44);
+            this.labelMode.Name = "labelMode";
+            this.labelMode.Size = new System.Drawing.Size(250, 33);
+            this.labelMode.TabIndex = 2;
+            this.labelMode.Text = "Выберите Режим";
+            // 
+            // labelWord
+            // 
+            this.labelWord.AutoSize = true;
+            this.labelWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelWord.Location = new System.Drawing.Point(50, 38);
+            this.labelWord.Name = "labelWord";
+            this.labelWord.Size = new System.Drawing.Size(36, 39);
+            this.labelWord.TabIndex = 3;
+            this.labelWord.Text = "?";
+            this.labelWord.Visible = false;
+            this.labelWord.Click += new System.EventHandler(this.labelWord_Click);
+            // 
+            // AnimalsPicture
+            // 
+            this.AnimalsPicture.Image = ((System.Drawing.Image)(resources.GetObject("AnimalsPicture.Image")));
+            this.AnimalsPicture.Location = new System.Drawing.Point(213, 81);
+            this.AnimalsPicture.Name = "AnimalsPicture";
+            this.AnimalsPicture.Size = new System.Drawing.Size(348, 217);
+            this.AnimalsPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.AnimalsPicture.TabIndex = 4;
+            this.AnimalsPicture.TabStop = false;
+            this.AnimalsPicture.Visible = false;
+            this.AnimalsPicture.Click += new System.EventHandler(this.AnimalsPicture_Click);
+            // 
+            // buttonQuestion2
+            // 
+            this.buttonQuestion2.Location = new System.Drawing.Point(630, 107);
+            this.buttonQuestion2.Name = "buttonQuestion2";
+            this.buttonQuestion2.Size = new System.Drawing.Size(75, 23);
+            this.buttonQuestion2.TabIndex = 5;
+            this.buttonQuestion2.UseVisualStyleBackColor = true;
+            this.buttonQuestion2.Visible = false;
+            this.buttonQuestion2.Click += new System.EventHandler(this.buttonQuestion2_Click);
+            // 
+            // buttonQuestion1
+            // 
+            this.buttonQuestion1.Location = new System.Drawing.Point(630, 81);
+            this.buttonQuestion1.Name = "buttonQuestion1";
+            this.buttonQuestion1.Size = new System.Drawing.Size(75, 23);
+            this.buttonQuestion1.TabIndex = 6;
+            this.buttonQuestion1.UseVisualStyleBackColor = true;
+            this.buttonQuestion1.Visible = false;
+            this.buttonQuestion1.Click += new System.EventHandler(this.buttonQuestion1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonQuestion1);
+            this.Controls.Add(this.buttonQuestion2);
+            this.Controls.Add(this.AnimalsPicture);
+            this.Controls.Add(this.labelWord);
+            this.Controls.Add(this.labelMode);
+            this.Controls.Add(this.buttonTest);
+            this.Controls.Add(this.buttonTrain);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.AnimalsPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonTrain;
+        private System.Windows.Forms.Button buttonTest;
+        private System.Windows.Forms.Label labelMode;
+        private System.Windows.Forms.Label labelWord;
+        private System.Windows.Forms.PictureBox AnimalsPicture;
+        private System.Windows.Forms.Button buttonQuestion2;
+        private System.Windows.Forms.Button buttonQuestion1;
+    }
+}
+
---
+++ animals4/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace animals4
+{
+    
+    public partial class Form1 : Form
+    {
+        string correctReply;
+
+        int idx = 0; // это счетчик для переключения надписей и картинок
+        string[] game = { "рувас", "карк", "тархъус" }; // это данные для обучения 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonTrain_Click(object sender, EventArgs e)
+        {
+            buttonTrain.Text = "Далее";
+           
+            
+           // buttonTrain.Location = new Point(295, 300);
+            buttonTrain.Left = 400;
+            buttonTrain.Top = 400;
+           // buttonTrain.Right = 150;
+            
+            buttonTest.Visible = false;
+            labelMode.Text = "Обучение";
+            AnimalsPicture.Visible = true;
+             labelWord.Visible = true;
+            string[] dirs = Directory.GetDirectories(@"C:\animals_images\");// список каталога с животными и путь к нему
+            string imagePath = dirs[idx] + @"\image.JPEG"; //путь к картинке "C:\animals_image\fox\image.png"
+            string wordFileName = dirs[idx] + @"\os.txt";// подобрали путь к текстовому файлу
+            string word = File.ReadAllText(wordFileName);//прочитали текст в os.txt             
+            AnimalsPicture.ImageLocation = imagePath; //сменили картинку
+            labelWord.Text = word; // это меняем текст рядом с изображением
+            idx++;              
+            if (idx + 1 == dirs.Length)
+            {
+                idx = 0;
+                MessageBox.Show("Вы прошли обучение");
+            }
+            
+                
+        }
+
+        private void AnimalsPicture_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelWord_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonTest_Click(object sender, EventArgs e)
+        {
+            buttonTest.Visible = false;
+            labelMode.Text = "Режим Тест";
+            buttonTrain.Visible = false;
+            AnimalsPicture.Visible = true;
+            labelWord.Visible = true;
+            buttonQuestion1.Visible = true;
+            buttonQuestion2.Visible = true;
+            string[] dirs = Directory.GetDirectories(@"C:\animals_images\");// список каталога с животными и путь к нему
+            string imagePath = dirs[idx] + @"\image.JPEG"; //путь к картинке "C:\animals_image\fox\image.png"
+            string wordFileName = dirs[idx] + @"\os.txt";// подобрали путь к текстовому файлу
+            string word = File.ReadAllText(wordFileName);//прочитали текст в os.txt 
+            labelWord.Text = word;
+            var rand = new Random();
+
+            int randomCorrect = rand.Next(0, dirs.Length);
+            int randomInvalid = rand.Next(0, dirs.Length);
+            while (true)
+            {
+                if (randomCorrect == randomInvalid)
+                {
+                    randomCorrect = rand.Next(0, dirs.Length);                    
+                }
+                else
+                {
+                    break;
+                }
+            }
+            //string imagePath = dirs[randomCorrect] + @"\image.JPEG";
+            AnimalsPicture.ImageLocation = imagePath;
+
+            var buttons = new Button[] { buttonQuestion1, buttonQuestion2 };
+            int correctButtonIndex = rand.Next(0, 2);
+            int invalidButtonIndex;
+            if(correctButtonIndex == 0)
+            {
+                invalidButtonIndex = 1;
+               
+            }
+            else
+            {
+                invalidButtonIndex = 0;
+            }
+
+            string correct = dirs[randomCorrect] + @"\os.txt";
+            string correctWord = File.ReadAllText(correct);
+            buttons[correctButtonIndex].Text = correctWord;
+            correctReply = correctWord;
+
+            string invalid = dirs[randomInvalid] + @"\os.txt";
+            string invalidWord = File.ReadAllText(invalid);
+            buttons[invalidButtonIndex].Text = invalidWord;
+
+        }
+
+        private void buttonQuestion2_Click(object sender, EventArgs e)
+        {
+
+            Button btn = sender as Button;
+            //MessageBox.Show("btn.Text = " + btn.Text + "correctReply = " + correctReply);
+            if (btn.Text == correctReply)
+            {
+                var rand = new Random();
+                string[] dirs = Directory.GetDirectories(@"C:\animals_images\");
+                int dirIndex = rand.Next(0, dirs.Length);                
+                //MessageBox.Show("" + dirIndex);
+                string imageFullName = dirs[dirIndex] + @"\image.JPEG";
+                AnimalsPicture.ImageLocation = imageFullName;
+
+                string correct = dirs[dirIndex] + @"\os.txt";
+                string correctWord = File.ReadAllText(correct);
+                buttonQuestion1.Text = correctWord;
+                correctReply = correctWord;
+
+                int invalidDirIndex = rand.Next(0, dirs.Length);
+                while (true)
+                {
+                    if (dirIndex == invalidDirIndex)
+                    {
+                        invalidDirIndex = rand.Next(0, dirs.Length);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                string invalid = dirs[invalidDirIndex] + @"\os.txt";
+                string invalidWord = File.ReadAllText(invalid);
+                var buttons = new Button[] { buttonQuestion1, buttonQuestion2 };
+                int correctButtonIndex = rand.Next(0, 2);
+                int invalidButtonIndex;
+                if (correctButtonIndex == 0)
+                {
+                    invalidButtonIndex = 1;
+
+                }
+                else
+                {
+                    invalidButtonIndex = 0;
+
+                }
+                buttons[invalidButtonIndex].Text = invalidWord;
+                buttons[correctButtonIndex].Text = correctWord;
+
+            }
+            else
+            {
+                MessageBox.Show("неверно");
+            }
+
+           
+        }
+
+        private void buttonQuestion1_Click(object sender, EventArgs e)
+        {
+
+            Button btn = sender as Button;
+           // MessageBox.Show("btn.Text = " + btn.Text + "correctReply = " + correctReply);
+
+            if (btn.Text == correctReply)
+            {
+            
+                var rand = new Random();
+                string[] dirs = Directory.GetDirectories(@"C:\animals_images\");
+                int dirIndex = rand.Next(0, dirs.Length);
+                //MessageBox.Show("" + dirIndex);
+                string imageFullName = dirs[dirIndex] + @"\image.JPEG";
+                AnimalsPicture.ImageLocation = imageFullName;
+
+                string correct = dirs[dirIndex] + @"\os.txt";
+                string correctWord = File.ReadAllText(correct);
+
+                correctReply = correctWord;
+
+                int invalidDirIndex = rand.Next(0, dirs.Length);
+                while (true)
+                {
+                    if (dirIndex == invalidDirIndex)
+                    {
+                        invalidDirIndex = rand.Next(0, dirs.Length);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                string invalid = dirs[invalidDirIndex] + @"\os.txt";
+                string invalidWord = File.ReadAllText(invalid);
+                var buttons = new Button[] { buttonQuestion1, buttonQuestion2 };
+                int correctButtonIndex = rand.Next(0, 2);
+                int invalidButtonIndex;
+                if (correctButtonIndex == 0)
+                {
+                    invalidButtonIndex = 1;
+
+                }
+                else
+                {
+                    invalidButtonIndex = 0;
+
+                }
+                buttons[invalidButtonIndex].Text = invalidWord;
+                buttons[correctButtonIndex].Text = correctWord;
+            }
+            else
+            {
+                MessageBox.Show("No Correct");
+            }
+            
+        }
+    }
+}
